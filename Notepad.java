ed7d63d9da42733a1873a8a830962fe8a782a993
TEA-14 <46442@students.riphah.edu.pk>
2024-10-29 21:18:14 +0500

    added border layout and status bar

--- Notepad.java
+++ Notepad.java
@@ -12,8 +12,11 @@ class Notepad {
         area = new JTextArea();
         JScrollPane scrollPane = new JScrollPane(area);
 
+        JLabel statusBar = new JLabel("Status Bar");
+
         frame.setLayout(new BorderLayout());
         frame.add(scrollPane, BorderLayout.CENTER);
+        frame.add(statusBar, BorderLayout.SOUTH);
         frame.setSize(800,600);
         frame.setVisible(true);
     }
